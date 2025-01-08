bf6ba400ed98082713b4f651e94db64d68debe86
FjellOverflow <fjelloverflow@protonmail.com>
2025-01-08 12:25:23 +0200

    add tour.ts

---
+++ src/plugins/tours.ts
@@ -0,0 +1,14 @@
+import { TourGuideClient } from '@sjmc11/tourguidejs'
+import '@sjmc11/tourguidejs/dist/css/tour.min.css'
+import type { TourGuideStep } from '@sjmc11/tourguidejs/src/types/TourGuideStep'
+
+const tour = new TourGuideClient()
+const tourGroupKeys = new Set<string>()
+
+export function useTour(group: string, steps: TourGuideStep[]) {
+  if (!tourGroupKeys.has(group)) {
+    tour.addSteps(steps)
+    tourGroupKeys.add(group)
+  }
+  return () => tour.start(group)
+}
