fa0a583dee532da565fa58ddbc09384ffde296f3
FjellOverflow <fjelloverflow@protonmail.com>
2025-01-14 10:24:14 +0200

    feat: filter lists by tags

--- src/components/Lists.vue
+++ src/components/Lists.vue
@@ -20,19 +20,36 @@ const props = withDefaults(
 )
 
 const filterKeyword = useRouteQuery('keyword', '')
+const filterTag = useRouteQuery('tag', 'any')
 const sortBy = useRouteQuery<'name' | 'difficulty'>('sortBy', 'name')
 const sortDirection = useRouteQuery<'asc' | 'desc'>('sortDir', 'asc')
+
+const availableTags = computed(() => [
+  'any',
+  ...new Set(
+    props.lists
+      .map((l) => l.meta.tags)
+      .flat()
+      .sort(),
+  ),
+])
 
 function toggleSortDirection() {
   sortDirection.value = sortDirection.value === 'asc' ? 'desc' : 'asc'
 }
 
 const filtersActive = computed(
-  () => !!filterKeyword.value || sortBy.value !== 'name' || sortDirection.value !== 'asc',
+  () =>
+    !!filterKeyword.value ||
+    sortBy.value !== 'name' ||
+    sortDirection.value !== 'asc' ||
+    filterTag.value !== 'any',
 )
 
 const filteredLists = computed(() => {
-  const sets = props.lists.filter((list) => matchKeyword(list.meta.name, filterKeyword.value))
+  const sets = props.lists
+    .filter((list) => matchKeyword(list.meta.name, filterKeyword.value))
+    .filter((list) => filterTag.value === 'any' || list.meta.tags.includes(filterTag.value))
 
   sets.sort((s1, s2) => {
     const [left, right] = sortDirection.value === 'desc' ? [s2, s1] : [s1, s2]
@@ -47,6 +64,7 @@ const filteredLists = computed(() => {
 
 function resetFilters() {
   filterKeyword.value = ''
+  filterTag.value = 'any'
   sortBy.value = 'name'
   sortDirection.value = 'asc'
 }
@@ -59,6 +77,11 @@ function resetFilters() {
       class="input input-bordered w-48"
       v-model="filterKeyword"
     />
+    <select class="select select-bordered w-48 capitalize" v-model="filterTag">
+      <option v-for="tag in availableTags" :key="tag" :value="tag" class="capitalize">
+        Tag: {{ tag }}
+      </option>
+    </select>
     <select class="select select-bordered w-48" v-model="sortBy">
       <option value="name">Sort by name</option>
       <option value="difficulty">Sort by difficulty</option>
