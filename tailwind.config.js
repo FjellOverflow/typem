cc3a51e7d08457d87c4e932ff5305ae77651bbee
FjellOverflow <fjelloverflow@protonmail.com>
2025-01-18 11:20:25 +0200

    refactor: remove unused file extensions from tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ import daisyui from 'daisyui'
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{html,md,js,ts,jsx,tsx,vue}'],
+  content: ['./src/**/*.{html,js,ts,vue}'],
   theme: {
     extend: {},
   },
