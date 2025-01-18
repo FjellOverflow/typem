b2efa40aea3c14f3486c461e361ca7d22284baa6
FjellOverflow <fjelloverflow@protonmail.com>
2025-01-18 11:28:56 +0200

    refactor: remove unused route meta from router

--- src/plugins/router.ts
+++ src/plugins/router.ts
@@ -2,12 +2,6 @@ import { createRouter, createWebHashHistory } from 'vue-router'
 import { routes } from 'vue-router/auto-routes'
 import { setupLayouts } from 'virtual:generated-layouts'
 
-declare module 'vue-router' {
-  interface RouteMeta {
-    title?: string
-  }
-}
-
 const router = createRouter({
   history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [...setupLayouts(routes), { path: '/:pathMatch(.*)*', redirect: '/' }],
