ac3df9f27e7af120d48527ec00089357c6427953
FjellOverflow <fjelloverflow@protonmail.com>
2025-02-27 14:16:30 +0200

    feat: option to show raw list object in listInfoPopUp

--- src/components/popUps/ListInfoPopUp.vue
+++ src/components/popUps/ListInfoPopUp.vue
@@ -1,21 +1,48 @@
 <script setup lang="ts">
 import type { IList } from '@/types'
-import { ExternalLinkIcon } from 'lucide-vue-next'
+import { FileCodeIcon, ExternalLinkIcon } from 'lucide-vue-next'
 
 const popUp = useTemplateRef('popUp')
 
-defineExpose({ open: () => popUp.value?.open() })
+defineExpose({
+  open: () => {
+    showRaw.value = false
+    return popUp.value?.open()
+  },
+})
 
 defineProps<{
   list: IList
 }>()
+
+const showRaw = ref(false)
 </script>
 <template>
   <PopUp ref="popUp">
+    <div
+      class="tooltip tooltip-bottom absolute right-4 top-4"
+      :data-tip="showRaw ? $t('Hide raw') : $t('Show raw')"
+    >
+      <button
+        @click="showRaw = !showRaw"
+        class="btn btn-ghost"
+        :class="{ 'text-primary': showRaw }"
+      >
+        <FileCodeIcon />
+      </button>
+    </div>
+
     <template #title>
       {{ list.meta.name }}
     </template>
-    <template #body>
+
+    <template v-if="showRaw" #body>
+      <div class="bg-neutral rounded-lg p-4">
+        <code class="text-sm break-all">{{ JSON.stringify(list, null, 2) }}</code>
+      </div>
+    </template>
+
+    <template v-else #body>
       <div class="flex flex-col gap-2">
         <span class="opacity-40 text-xl"> {{ list.meta.tags.map((t) => `#${t}`).join(', ') }}</span>
         <span class="text-xl sm:text-2xl">{{ list.meta.description }}</span>
--- src/components/popUps/PopUp.vue
+++ src/components/popUps/PopUp.vue
@@ -6,9 +6,12 @@ defineExpose({ open: () => popUp.value?.showModal() })
 <template>
   <dialog ref="popUp" class="modal">
     <div class="modal-box border rounded-lg w-[80%] flex flex-col gap-2 sm:gap-4">
-      <div class="sm:text-4xl text-3xl">
+      <slot />
+
+      <div class="sm:text-4xl text-3xl w-[85%]">
         <slot name="title" />
       </div>
+
       <div class="sm:text-xl text-lg">
         <slot name="body" />
       </div>
