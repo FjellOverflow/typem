ee3f8a58151aa384e64407a14d551937cf04e5be
FjellOverflow <fjelloverflow@protonmail.com>
2025-03-16 15:27:21 +0200

    test(unit): useHotkey composable

---
+++ unit/hotkey.test.ts
@@ -0,0 +1,33 @@
+import { describe, it, expect, vi } from 'vitest'
+import { useHotkey } from '@/composables/hotkey'
+
+describe('hotkey composable', () => {
+  it('calls callback', async () => {
+    const callback = vi.fn()
+
+    useHotkey('f', callback)
+
+    const type = (key: string, ctrlKey: boolean) =>
+      document.dispatchEvent(new KeyboardEvent('keydown', { key, ctrlKey }))
+
+    new KeyboardEvent('keydown', { key: 'f', ctrlKey: true })
+
+    type('a', false)
+    expect(callback).toHaveBeenCalledTimes(0)
+
+    type('f', false)
+    expect(callback).toHaveBeenCalledTimes(0)
+
+    type('a', true)
+    expect(callback).toHaveBeenCalledTimes(0)
+
+    type('f', true)
+    expect(callback).toHaveBeenCalledTimes(1)
+
+    type('f', true)
+    expect(callback).toHaveBeenCalledTimes(2)
+
+    type('a', true)
+    expect(callback).toHaveBeenCalledTimes(2)
+  })
+})
