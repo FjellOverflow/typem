8311e71a4d5b299c47b44282f8c37d3d30d1238e
FjellOverflow <fjelloverflow@protonmail.com>
2025-03-16 15:53:04 +0200

    test(unit): useFavorites composable

---
+++ unit/favorites.test.ts
@@ -0,0 +1,48 @@
+import { describe, expect, it } from 'vitest'
+import { useFavorites } from '@/composables/favorites'
+
+const list0 = {
+  id: 'list0',
+  meta: {
+    name: '',
+    description: '',
+    difficulty: 0,
+    author: '',
+    lastUpdated: new Date().toISOString(),
+    source: '',
+    tags: [],
+    category: '',
+    isCustom: false,
+  },
+  items: [],
+  settings: {
+    requireWhitespaces: false,
+    requireCapitalization: false,
+    requireOrder: false,
+    numberOfPauses: 0,
+    showHints: false,
+    allowOverride: false,
+    shuffle: false,
+  },
+}
+
+const list1 = { ...list0, id: 'list1' }
+
+describe('favorites composable', () => {
+  it('toggles favorite', async () => {
+    const { toggleFavorite, isFavorite } = useFavorites()
+
+    expect(isFavorite(list0)).toBe(false)
+    expect(isFavorite(list1)).toBe(false)
+
+    toggleFavorite(list0)
+
+    expect(isFavorite(list0)).toBe(true)
+    expect(isFavorite(list1)).toBe(false)
+
+    toggleFavorite(list0)
+
+    expect(isFavorite(list0)).toBe(false)
+    expect(isFavorite(list1)).toBe(false)
+  })
+})
