6f7ab182a048aa3cd031629ac43eab70779cd7d0
FjellOverflow <fjelloverflow@protonmail.com>
2025-03-26 15:42:25 +0200

    test(e2e): more /settings tests

--- cypress/e2e/settings.cy.ts
+++ cypress/e2e/settings.cy.ts
@@ -57,9 +57,27 @@ describe('/settings', () => {
 
     dialog().find('button').contains('Upload').click()
 
+    cy.get('.toast').contains('Import successful').should('exist')
+
     cy.visit('/#/history')
     cy.contains('No playthroughs yet').should('not.exist')
     cy.get('.playthrough-card').contains('Planets of the Solar System').should('exist')
+  })
+
+  it("doesn't import malformed  history", () => {
+    historyCard().find('button').contains('Import from JSON').click()
+    const dialog = () => cy.get('dialog').contains('Overwrite history?').parentsUntil('dialog')
+
+    dialog()
+      .find('input[type="file"]')
+      .selectFile(Cypress.Buffer.from(JSON.stringify({})))
+
+    dialog().find('button').contains('Upload').click()
+
+    cy.get('.toast').contains('Import failed').should('exist')
+
+    cy.visit('/#/history')
+    cy.contains('No playthroughs yet').should('exist')
   })
 
   it('exports history', () => {
@@ -148,6 +166,17 @@ describe('/settings', () => {
       )
 
     dialog().find('button').contains('Upload').click()
+
+    cy.get('.toast').contains('Import successful').should('exist')
+
+    cy.get('div')
+      .contains('Custom lists')
+      .parent()
+      .parent()
+      .parent()
+      .contains('Our planet')
+      .should('exist')
+
     cy.visit('/')
     const card = () => cy.get('#list-preview-custom-our-planet')
 
@@ -156,6 +185,215 @@ describe('/settings', () => {
     card().contains('1 items').should('exist')
     card().contains('Easy').should('exist')
     card().contains('Custom').should('exist')
+  })
+
+  it("doesn't import malformed custom list", () => {
+    cy.get('div')
+      .contains('Custom lists')
+      .parent()
+      .parent()
+      .parent()
+      .find('button')
+      .contains('Import from JSON')
+      .click()
+
+    const dialog = () => cy.get('dialog').contains('Import custom list').parentsUntil('dialog')
+
+    dialog()
+      .find('input[type="file"]')
+      .selectFile(
+        Cypress.Buffer.from(
+          JSON.stringify({
+            meta: {
+              name: 'Our planet',
+              description: 'Name our planet',
+              difficulty: 1,
+              tags: ['universe'],
+              category: 'universe',
+              source: 'https://en.wikipedia.org/wiki/Solar_System',
+              author: 'FjellOverflow',
+              lastUpdated: '2025-03-25',
+            },
+            items: [
+              {
+                answer: 'Earth',
+                matches: ['Earth'],
+              },
+            ],
+            settings: {
+              requireWhitespaces: false,
+              requireCapitalization: false,
+              requireOrder: true,
+              numberOfPauses: 0,
+              showHints: false,
+              allowOverride: true,
+            },
+          }),
+        ),
+      )
+
+    dialog().find('button').contains('Upload').click()
+
+    cy.get('.toast').contains('Import failed').should('exist')
+  })
+
+  it('removes custom list', () => {
+    cy.window().then((window) => {
+      window.localStorage.setItem(
+        'customLists',
+        JSON.stringify([
+          {
+            id: 'our-planet',
+            meta: {
+              name: 'Our planet',
+              description: 'Name our planet',
+              difficulty: 1,
+              tags: ['universe'],
+              category: 'universe',
+              source: 'https://en.wikipedia.org/wiki/Solar_System',
+              author: 'FjellOverflow',
+              lastUpdated: '2025-03-25',
+            },
+            items: [
+              {
+                answer: 'Earth',
+                matches: ['Earth'],
+              },
+            ],
+            settings: {
+              requireWhitespaces: false,
+              requireCapitalization: false,
+              requireOrder: true,
+              numberOfPauses: 0,
+              showHints: false,
+              allowOverride: true,
+            },
+          },
+        ]),
+      )
+    })
+
+    cy.get('div')
+      .contains('Custom lists')
+      .parent()
+      .parent()
+      .parent()
+      .contains('Our planet')
+      .parent()
+      .find('div[data-tip="Delete custom list"]')
+      .find('button')
+      .click()
+
+    cy.get('dialog')
+      .contains('Once deleted, the list can not be recovered.')
+      .parent()
+      .parent()
+      .find('button')
+      .contains('Confirm')
+      .click()
+
+    cy.get('div')
+      .contains('Custom lists')
+      .parent()
+      .parent()
+      .parent()
+      .contains('Our planet')
+      .should('not.exist')
+
+    cy.visit('/')
+    cy.get('#list-preview-custom-our-planet').should('not.exist')
+  })
+
+  it('removes custom lists', () => {
+    cy.window().then((window) => {
+      window.localStorage.setItem(
+        'customLists',
+        JSON.stringify([
+          {
+            id: 'our-planet',
+            meta: {
+              name: 'Our planet',
+              description: 'Name our planet',
+              difficulty: 1,
+              tags: ['universe'],
+              category: 'universe',
+              source: 'https://en.wikipedia.org/wiki/Solar_System',
+              author: 'FjellOverflow',
+              lastUpdated: '2025-03-25',
+            },
+            items: [
+              {
+                answer: 'Earth',
+                matches: ['Earth'],
+              },
+            ],
+            settings: {
+              requireWhitespaces: false,
+              requireCapitalization: false,
+              requireOrder: true,
+              numberOfPauses: 0,
+              showHints: false,
+              allowOverride: true,
+            },
+          },
+          {
+            id: 'our-planet-2',
+            meta: {
+              name: 'Our planet 2',
+              description: 'Name our planet',
+              difficulty: 1,
+              tags: ['universe'],
+              category: 'universe',
+              source: 'https://en.wikipedia.org/wiki/Solar_System',
+              author: 'FjellOverflow',
+              lastUpdated: '2025-03-25',
+            },
+            items: [
+              {
+                answer: 'Earth',
+                matches: ['Earth'],
+              },
+            ],
+            settings: {
+              requireWhitespaces: false,
+              requireCapitalization: false,
+              requireOrder: true,
+              numberOfPauses: 0,
+              showHints: false,
+              allowOverride: true,
+            },
+          },
+        ]),
+      )
+    })
+
+    cy.get('div')
+      .contains('Custom lists')
+      .parent()
+      .parent()
+      .parent()
+      .find('button')
+      .contains('Remove all')
+      .click()
+
+    cy.get('dialog')
+      .contains('This action removes ALL custom lists. Once remoevd, they can not be recovered.')
+      .parent()
+      .parent()
+      .find('button')
+      .contains('Confirm')
+      .click()
+
+    cy.get('div')
+      .contains('Custom lists')
+      .parent()
+      .parent()
+      .parent()
+      .contains('Our planet')
+      .should('not.exist')
+
+    cy.visit('/')
+    cy.get('#list-preview-custom-our-planet').should('not.exist')
   })
 })
 
