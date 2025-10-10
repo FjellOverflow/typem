aa73b244011b526f27258919c6b31f37c650c23b
FjellOverflow <fjelloverflow@protonmail.com>
2025-10-10 12:03:28 +0200

    fix: page title

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,10 +1,13 @@
 <script lang="ts">
 import { useListsLoader } from '@/plugins/listLoader'
+import { useTitle } from '@vueuse/core'
 export { useListsLoader }
 </script>
 
 <script setup lang="ts">
 const { data: lists } = useListsLoader()
+
+useTitle('Typem')
 </script>
 <template>
   <ListsList :lists />
