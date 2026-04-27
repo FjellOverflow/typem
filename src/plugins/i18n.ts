8003a359c55ee75a77610951afbe684791af3bfd
FjellOverflow <fjelloverflow@protonmail.com>
2026-04-27 20:30:10 +0200

    fix: validate `locale` when read from storage

--- src/plugins/i18n.ts
+++ src/plugins/i18n.ts
@@ -1,9 +1,17 @@
 import { createI18n } from 'vue-i18n'
 import messages from '@/locales/index'
 
+const availableLocales = Object.keys(messages)
+
+const localeFromStorage = localStorage.getItem('locale')
+
+let locale = 'en'
+
+if (localeFromStorage && availableLocales.includes(localeFromStorage)) locale = localeFromStorage
+
 const i18n = createI18n({
   legacy: false,
-  locale: localStorage.getItem('locale') || 'en',
+  locale,
   messages,
 })
 
