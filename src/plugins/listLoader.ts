7ee5b847574260fdc61a4188d3894cdd69c7d92e
FjellOverflow <fjelloverflow@protonmail.com>
2026-04-27 21:14:12 +0200

    refactor: replace unneccessary promise with IIEF

--- src/plugins/listLoader.ts
+++ src/plugins/listLoader.ts
@@ -9,7 +9,7 @@ const builtInLists: IList[] = []
 
 const lists = computed(() => [...builtInLists, ...customLists.value])
 
-const isLoaded = new Promise(async (resolve) => {
+const isLoaded = (async () => {
   const files = import.meta.glob('@/assets/lists/**/[^.]*.json')
 
   const promises = Object.entries(files).map(([fileName, file]) =>
@@ -25,9 +25,7 @@ const isLoaded = new Promise(async (resolve) => {
 
   const results = await Promise.all(promises)
   builtInLists.push(...results.filter((r) => !!r))
-
-  resolve(true)
-})
+})()
 
 export async function getListById(listId: string): Promise<IList> {
   await isLoaded
